4e316a6b9bb29a8d8793f5eddc535313be912507
AchaUmbaPretre <achandambi@gmail.com>
2023-12-01 12:20:39 +0100

    commit

--- src/pages/rightbar/Rightbar.jsx
+++ src/pages/rightbar/Rightbar.jsx
@@ -1,3 +1,4 @@
+import RowChart from '../../components/rowChart/RowChart'
 import RowTotal from '../../components/rowTotal/RowTotal'
 import RowTotalDetail from '../../components/rowTotalDetail/RowTotalDetail'
 import './rightbar.scss'
@@ -9,6 +10,12 @@ const Rightbar = () => {
         <div className="right-wrapper">
           <RowTotal/>
           <RowTotalDetail/>
+          <div className="rightRows">
+            <div className="rightbar-left">
+              <RowChart/>
+            </div>
+            <div className="rightbar-right">2</div>
+          </div>
         </div>
       </div>
     </>
