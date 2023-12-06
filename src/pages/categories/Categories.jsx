ae2dd6631f336216f130e7e2920e6ba8cc71cf51
AchaUmbaPretre <achandambi@gmail.com>
2023-12-06 10:19:47 +0100

    commit

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,9 +1,198 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { PlusOutlined, SearchOutlined, SisternodeOutlined,EyeOutlined, FilePdfOutlined, FileExcelOutlined,EditOutlined, PrinterOutlined, DeleteOutlined} from '@ant-design/icons';
+import { Button, Input, Space, Table, Popover,Popconfirm} from 'antd';
+import React, { useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
 import './categories.scss'
 
 const Categories = () => {
     const navigate = useNavigate();
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const scroll = { x: 400 };
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+      };
+      const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+      };
+    
+      const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+          <div
+            style={{
+              padding: 8,
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+          >
+            <Input
+              ref={searchInput}
+              placeholder={`Search ${dataIndex}`}
+              value={selectedKeys[0]}
+              onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+              onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              style={{
+                marginBottom: 8,
+                display: 'block',
+              }}
+            />
+            <Space>
+              <Button
+                type="primary"
+                onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                icon={<SearchOutlined />}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Search
+              </Button>
+              <Button
+                onClick={() => clearFilters && handleReset(clearFilters)}
+                size="small"
+                style={{
+                  width: 90,
+                }}
+              >
+                Reset
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  confirm({
+                    closeDropdown: false,
+                  });
+                  setSearchText(selectedKeys[0]);
+                  setSearchedColumn(dataIndex);
+                }}
+              >
+                Filter
+              </Button>
+              <Button
+                type="link"
+                size="small"
+                onClick={() => {
+                  close();
+                }}
+              >
+                close
+              </Button>
+            </Space>
+          </div>
+        ),
+        filterIcon: (filtered) => (
+          <SearchOutlined
+            style={{
+              color: filtered ? '#1677ff' : undefined,
+            }}
+          />
+        ),
+        onFilter: (value, record) =>
+          record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+          if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+          }
+        },
+        render: (text) =>
+          searchedColumn === dataIndex ? (
+            <Highlighter
+              highlightStyle={{
+                backgroundColor: '#ffc069',
+                padding: 0,
+              }}
+              searchWords={[searchText]}
+              autoEscape
+              textToHighlight={text ? text.toString() : ''}
+            />
+          ) : (
+            text
+          ),
+      });
+
+    
+    const data = [
+        {
+          key: '2',
+          code: '2',
+          nom_produit: "Ketch",
+          couleur: 'red',
+          categorie: "chaussure plate"
+        },
+        {
+          key: '3',
+          code: '3',
+          nom_produit: "Ketch",
+          couleur: 'red',
+          categorie: "chaussure plate"
+        },
+        {
+          key: '4',
+          code: '4',
+          nom_produit: "Ketch",
+          couleur: 'red',
+          categorie: "chaussure plate"
+        },
+      ];
+
+      const columns = [
+        {
+            title: 'Categorie',
+            dataIndex: 'categorie',
+            key: 'categorie',
+            
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            width: '10%',
+            render: (text, record) => (
+                
+              <Space size="middle">
+                <Popconfirm
+                  title="Êtes-vous sûr de vouloir modifier?"
+                  onConfirm={()=> handleEdit(record.id)}
+                  okText="Oui"
+                  cancelText="Non"
+                >
+                  <Button icon={<EditOutlined />} style={{ color: 'green' }} />
+                </Popconfirm>
+                <Link to={`/presenceListView/${record.emp1_id}`}>
+                  <Button icon={<EyeOutlined />} style={{ color: 'blue' }} />
+                </Link>
+                <Popconfirm
+                  title="Êtes-vous sûr de vouloir supprimer?"
+                  onConfirm={() => handleDelete(record.id)}
+                  okText="Oui"
+                  cancelText="Non"
+                >
+                  <Button icon={<DeleteOutlined />} style={{ color: 'red' }} />
+                </Popconfirm>
+              </Space>
+            ),
+          },
+      ];
+
+
+      const handleEdit = (id) => {
+        navigate(`/presenceEdit/${id}`);
+    };
+    
+    const handleDelete = async (id) => {
+     /*  try {
+        await axios.delete(`${DOMAIN}/api/admin/presence/${id}`);
+          window.location.reload();
+      } catch (err) {
+        console.log(err);
+      } */
+    };
 
   return (
     <>
@@ -21,7 +210,19 @@ const Categories = () => {
                         <button className="categorie-btn">Envoyer</button>
                     </div>
                     <div className="categorie-container-right">
-                        BBB
+                        <div className="categorie-right-top">
+                            <div className="categorie-left">
+                                <FilePdfOutlined className='product-icon-pdf' />
+                                <FileExcelOutlined className='product-icon-excel'/>
+                                <PrinterOutlined className='product-icon-printer'/>
+                            </div>
+                            <div className="categorie-right">
+                                <input type="search" name="" id="" placeholder='Recherche...' className='categorie-search' />
+                            </div>
+                        </div>
+                        <div className="categorie-right-bottom">
+                            <Table columns={columns} dataSource={data} scroll={scroll} pagination={{ pageSize: 5}} />
+                        </div>
                     </div>
                 </div>
 
