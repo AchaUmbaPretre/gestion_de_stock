a3f0dc66beb54f55b52b9e95bc005426f523a8cb
AchaUmbaPretre <achandambi@gmail.com>
2023-12-06 12:26:15 +0100

    commit

---
+++ src/pages/rapportVente.jsx/RapportVente.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './rapportVente.scss'
+
+const rapportVente = () => {
+  return (
+    <>
+        <div className="rapportVente">
+            <div className="rapportVente-wrapper">
+                <div className="rapportVente-container">
+                    
+                </div>
+            </div>
+        </div>
+
+    </>
+  )
+}
+
+export default rapportVente
