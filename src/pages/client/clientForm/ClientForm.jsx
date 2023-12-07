b5c55f4ffed82c94f22ecd0ff5a3fde10e1840a6
AchaUmbaPretre <achandambi@gmail.com>
2023-12-07 13:25:15 +0100

    commit

--- src/pages/client/clientForm/ClientForm.jsx
+++ src/pages/client/clientForm/ClientForm.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react';
 import './clientForm.scss'
 import { CloudUploadOutlined  } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import config from '../../../config';
 
 const ClientForm = () => {
+  const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+  const [data, setData] = useState({})
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+  
+    let updatedValue = fieldValue;
+  
+    if (fieldName === "contact_email") {
+      updatedValue = fieldValue.toLowerCase();
+    } else if (Number.isNaN(Number(fieldValue))) {
+      updatedValue = fieldValue.charAt(0).toUpperCase() + fieldValue.slice(1);
+    }
+  
+  setData((prev) => ({ ...prev, [fieldName]: updatedValue }));
+  };
+
+  console.log(data)
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try{
+      await axios.post(`${DOMAIN}/api/peuple/client`, data)
+      Swal.fire({
+        title: 'Success',
+        text: 'Client crée avec succès!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      });
+      navigate('/clients')
+      window.location.reload();
+
+    }catch(err) {
+      Swal.fire({
+        title: 'Error',
+        text: err.message,
+        icon: 'error',
+        confirmButtonText: 'OK',
+      });
+    }
+  }
+
   return (
     <>
         <div className="clientForm">
@@ -17,34 +66,36 @@ const ClientForm = () => {
               <div className="product-container-bottom">
                 <div className="form-controle">
                   <label htmlFor="">Nom</label>
-                  <input type="text" className="form-input"  required/>
+                  <input type="text" name='nom' className="form-input" onChange={handleInputChange}  required/>
                 </div>
                 <div className="form-controle">
                   <label htmlFor="">Raison sociale</label>
-                  <select name="" id="" className="form-input">
+                  <select id="" className="form-input" name="raison_sociale" onChange={handleInputChange}>
                     <option value="" disabled>Selectionnez une raison sociale</option>
+                    <option value="Client VIP">client VIP</option>
+                    <option value="Client Normal">client Normal</option>
                   </select>
                 </div>
                 <div className="form-controle">
                   <label htmlFor="">Email</label>
-                  <input type="email" className="form-input" />
+                  <input type="email" name='email' className="form-input" onChange={handleInputChange} />
                 </div>
                 <div className="form-controle">
                   <label htmlFor="">Telephone</label>
-                  <input type="tel" className="form-input" />
+                  <input type="tel" name='telephone' className="form-input" onChange={handleInputChange} />
                 </div>
                 <div className="form-controle">
                   <label htmlFor="">Ville</label>
-                  <input type="number" className="form-input" />
+                  <input type="text" name="ville" className="form-input" onChange={handleInputChange} />
                 </div>
                 <div className="form-controle">
                   <label htmlFor="">Adresse</label>
-                  <input type="number" className="form-input" />
+                  <input type="text" name="adresse" className="form-input" onChange={handleInputChange} />
                 </div>
               </div>
 
               <div className="form-submit">
-                <button className="btn-submit">Soumetre</button>
+                <button className="btn-submit" onClick={handleClick}>Soumetre</button>
                 <button className="btn-submit btn-annuler">Annuler</button>
               </div>
             </div>
