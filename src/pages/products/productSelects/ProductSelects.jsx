36f76a5fe96c841cfbb7bdbd2e93a396994acd49
AchaUmbaPretre <achandambi@gmail.com>
2023-12-13 09:59:42 +0100

    commit

--- src/pages/products/productSelects/ProductSelects.jsx
+++ src/pages/products/productSelects/ProductSelects.jsx
@@ -1,36 +1,79 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import './productSelects.scss'
 import { SearchOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import config from './../../../config';
+import Select from 'react-select';
 
 const ProductSelects = () => {
+    const DOMAIN = config.REACT_APP_SERVER_DOMAIN;
+    const [produit, setProduit] = useState([]);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+
+    const handleInputChange = (e) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+      
+        let updatedValue = fieldValue;
+      
+        if (fieldName === "contact_email") {
+          updatedValue = fieldValue.toLowerCase();
+        } else if (Number.isNaN(Number(fieldValue))) {
+          updatedValue = fieldValue.charAt(0).toUpperCase() + fieldValue.slice(1);
+        }
+      
+      setData((prev) => ({ ...prev, [fieldName]: updatedValue }));
+      };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`${DOMAIN}/api/produit`);
+        setProduit(data);
+        setLoading(false)
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data)
+
   return (
     <>
         <div className="productSelects">
             <div className="productSelects-container">
-                <select name="" id="" className="product-input-select">
-                    <option value="" disabled selected>
-                        Choisir un produit
-                    </option>
-                    <option value="">aaaaaa</option>
-                </select>
-                <select name="" id="" className="product-input-select">
-                    <option value="" disabled selected>
-                        Choisir une catégorie
-                    </option>
-                    <option value="">aaaaaa</option>
-                </select>
-                <select name="" id="" className="product-input-select">
-                    <option value="" disabled selected>
-                        Choisir une couleur
-                    </option>
-                    <option value="">aaaaaa</option>
-                </select>
-                <select name="" id="" className="product-input-select">
-                    <option value="" disabled selected>
-                        Prix
-                    </option>
-                    <option value="">aaaaaa</option>
-                </select>
+                <Select
+                    className="product-input-select"
+                    name='produit_id'
+                    options={produit?.map(item => ({ value: item.produit_id, label: item.nom_produit }))}
+                    onChange={selectedOption => handleInputChange({ target: { name: 'produit_id', value: selectedOption.value } })}
+                    placeholder="Choisir un produit"
+                />
+                 <Select
+                    className="product-input-select"
+                    name='categorie'
+                    options={produit?.map(item => ({ value: item.categorie, label: item.nom_categorie }))}
+                    onChange={selectedOption => handleInputChange({ target: { name: 'categorie', value: selectedOption.value } })}
+                    placeholder="Choisir une categorie"
+                />
+                <Select
+                    className="product-input-select"
+                    name='couleur'
+                    options={produit?.map(item => ({ value: item.couleur, label: item.nom_couleur }))}
+                    onChange={selectedOption => handleInputChange({ target: { name: 'couleur', value: selectedOption.value } })}
+                    placeholder="Choisir une couleur"
+                />
+                <Select
+                    className="product-input-select"
+                    name='prix'
+                    options={produit?.map(item => ({ value: item.prix, label: item.prix }))}
+                    onChange={selectedOption => handleInputChange({ target: { name: 'prix', value: selectedOption.value } })}
+                    placeholder="Prix"
+                />
                 <div className="select-btn">
                     <SearchOutlined className='select-search-btn' />
                 </div>
