480e585f14fab8d450bfbeea708cd181ffa09064
AchaUmbaPretre <achandambi@gmail.com>
2023-12-15 16:20:39 +0100

    commit

--- src/components/rowTotalDetail/RowTotalDetail.jsx
+++ src/components/rowTotalDetail/RowTotalDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './rowTotalDetail.scss'
-import { UsergroupAddOutlined, UserOutlined, SnippetsOutlined,AuditOutlined } from '@ant-design/icons';
+import { UsergroupAddOutlined, UserOutlined, SnippetsOutlined,ShoppingOutlined } from '@ant-design/icons';
 import CountUp from 'react-countup';
 import config from '../../config';
 import { useNavigate } from 'react-router-dom';
@@ -84,10 +84,10 @@ const RowTotalDetail = () => {
                 <div className="rowTotalDetail-row" style={{background: 'rgba(0, 128, 0, 0.74)'}}>
                     <div className="rowTotalDetail-left">
                         <h2 className="rowTotal-h2">5</h2>
-                        <span className="rowTotal-span">Facture de vente</span>
+                        <span className="rowTotal-span">vente</span>
                     </div>
                     <div className="rowTotalDetail-right">
-                        <AuditOutlined className='rowTotalIcon'/>
+                        <ShoppingOutlined className='rowTotalIcon'/>
                     </div>
                 </div>
             </div>
